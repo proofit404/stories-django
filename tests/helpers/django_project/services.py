1848a583c9ec1546488d87d16a5d6eb6a9c9ec34
Artem Malyshev <proofit404@gmail.com>
2021-03-17 01:07:35 +0300

    f

---
+++ tests/helpers/django_project/services.py
@@ -0,0 +1,54 @@
+from stories import arguments
+from stories import Result
+from stories import story
+from stories import Success
+
+
+class Action:
+    """Service."""
+
+    @story
+    @arguments("foo", "bar")
+    def do(I):
+        """Define service."""
+        I.one
+        I.two
+        I.three
+
+    def one(self, state):
+        """Do step."""
+        state.baz = state.foo + state.bar
+        return Success()
+
+    def two(self, state):
+        """Do step."""
+        state.quiz = state.baz * 2
+        return Success()
+
+    def three(self, state):
+        """Do step."""
+        state.result = state.quiz - 1
+        return Result(state.result)
+
+
+class Admin:
+    """Service."""
+
+    @story
+    def do(I):
+        """Define service."""
+        I.x
+        I.y
+        I.z
+
+    def x(self, state):
+        """Do step."""
+        return Success()
+
+    def y(self, state):
+        """Do step."""
+        return Success()
+
+    def z(self, state):
+        """Do step."""
+        return Success()
--- tests/helpers/django_project/urls.py
+++ tests/helpers/django_project/urls.py
@@ -5,11 +5,15 @@ from django.urls import include
 from django.urls import path
 from django.urls import re_path
 
+from django_project.views import AdminView
 from django_project.views import IndexView
+from django_project.views import StoryView
 
 
 urlpatterns = [
     re_path(r"^static/(?P<path>.*)$", serve),
     path("__debug__/", include(debug_toolbar.urls)),
     path("", IndexView.as_view()),
+    path("story/", StoryView.as_view()),
+    path("admin/", AdminView.as_view()),
 ]
